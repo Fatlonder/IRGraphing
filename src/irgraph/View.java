f4ef46bf29bddd4457df7c4a0525bb323a0648f9
Fatlonder <ATQU@DESKTOP-2M97LSN>
2016-05-09 19:25:54 +0200

    Latest update

---
+++ src/irgraph/View.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irgraph;
+
+/**
+ *
+ * @author FA
+ */
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import gnu.io.*;
+import java.awt.event.ActionListener;
+import java.util.Enumeration;
+
+public class View extends JFrame {
+
+    private JFrame window = new JFrame();
+    private JComboBox<String> portList = new JComboBox<String>();
+    private JButton connectButton = new JButton("Connect");
+    private JButton clearButton = new JButton("Clear");
+    private Model theModel = new Model();
+
+    View() {
+        JPanel topPanel = new JPanel();
+        window.setTitle("IR Discrete Values");
+        window.setSize(600, 400);
+        window.setLayout(new BorderLayout());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        topPanel.add(portList);
+        topPanel.add(connectButton);
+        topPanel.add(clearButton);
+        window.add(topPanel, BorderLayout.NORTH);
+
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            String currPort = currPortId.getName();
+            portList.addItem(currPort);
+
+        }
+        //dataset.addSeries(series);
+        //model.getDataset().addSeries(model.getSeries());
+        //JFreeChart chart = ChartFactory.createXYStepChart("IR Data", "Time (ms)", "Bit", theModel.dataset());
+        //window.add(new ChartPanel(chart), BorderLayout.CENTER);
+
+        // configure the connect button 
+        // Listener for array size
+        //show the window
+        window.setVisible(true);
+
+    }
+
+    public JFrame getWindow() {
+        return window;
+    }
+
+    public JComboBox<String> getPortList() {
+        return portList;
+    }
+
+    public void connectListener(ActionListener ListenForButton) {
+        connectButton.addActionListener(ListenForButton);
+    }
+
+    public void clearListener(ActionListener ListenForButton) {
+        clearButton.addActionListener(ListenForButton);
+    }
+
+    /* public void populatePortList(ActionListener ListenForButton) {
+     portList.addActionListener(ListenForButton);
+     }*/
+    public JButton getConnectButton() {
+        return connectButton;
+    }
+
+}
