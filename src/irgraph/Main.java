5358863014975876c5cefbb07e298ec27ffbfe3b
Fatlonder <ATQU@DESKTOP-2M97LSN>
2016-05-09 02:21:31 +0200

    Latest update

---
+++ src/irgraph/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irgraph;
+
+/**
+ *
+ * @author FA
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Model theModel = new Model();
+        View theView = new View();
+        Controller theController = new Controller(theModel, theView);
+
+    }
+}
